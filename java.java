e3d86603568ac72f2028412f42214687c76c7088
patittawm <spicielover@gmail.com>
2022-04-10 08:58:20 -0700

    finance Task1

---
+++ java.java
@@ -0,0 +1,22 @@
+package day4;
+
+public class FruitsFarmColors {
+
+	public static void main(String[] args) {
+		String apple = "green";
+		String babana = "yellow";
+		String orange = "light orange";
+		String kiwi = "brown";
+		String pear = "yellow";
+		System.out.println("Apple color : " + apple);
+		System.out.println("Banana color : " + babana);
+		System.out.println("Orange color: " + orange);
+		System.out.println("Kiwi color : " + kiwi);
+		System.out.println("Pear colot : " + pear);
+		
+		String allFruits = apple + "," + babana + "," + orange + "," + kiwi + "," + pear;
+		System.out.println("Fruits : " + allFruits);
+
+	}
+
+}
